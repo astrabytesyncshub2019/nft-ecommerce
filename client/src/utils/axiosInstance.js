6f38ab0ae1f021f7a6eecc519a087316c2cc6b87
Mann <randhawamanpreet37@gmail.com>
2025-09-22 13:55:22 +0530

    backend url setted

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://mern-ecommerce-agxk.onrender.com",
     timeout: 10000,
     withCredentials: true
 })
