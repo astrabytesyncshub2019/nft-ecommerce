88d44c09e6a74d257fe757444b6009cf62697504
Mann <randhawamanpreet37@gmail.com>
2025-09-22 14:00:16 +0530

    Fix CORS to allow Vercel frontend

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: (origin, callback) => {
     const allowedOrigins = [         // local dev
-      "https://scatch-bice.vercel.app/"  // deployed frontend
+      "https://scatch-bice.vercel.app"  // deployed frontend
     ]
 
     if (!origin || allowedOrigins.includes(origin)) {
