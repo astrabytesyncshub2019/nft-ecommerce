12a83bc48131b6f62e9d6b6fe275722f9d57209e
Mann <randhawamanpreet37@gmail.com>
2025-09-24 01:05:40 +0530

    DEPLOYMENT

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL
 // console.log(import.meta.env.VITE_API_URL)
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: "/",
     timeout: 10000,
     withCredentials: true
 })
--- server/src/config/cookieOptions.js
+++ server/src/config/cookieOptions.js
@@ -1,17 +1,17 @@
-const isProduction = process.env.NODE_ENV === "production"
+// const isProduction = process.env.NODE_ENV === "production"
 
 export const cookieOptionsForAcessToken = {
   httpOnly: true,
-  secure: isProduction,                    // must be true in prod (HTTPS)
-  sameSite: "None",                        // "None" required for cross-site
-  maxAge: 180 * 60 * 1000,                 // 3 hours
+  secure: true,                    // must be true in prod (HTTPS)
+  sameSite: "strict",                        // "None" required for cross-site
+  maxAge: 180 * 60 * 1000,                 
   path: "/",
 }
 
 export const cookieOptionsForRefreshToken = {
   httpOnly: true,
-  secure: isProduction,
-  sameSite: "None",                        // must be None
+  secure: true,
+  sameSite: "strict",                        // must be None
   maxAge: 7 * 24 * 60 * 60 * 1000,         // 7 days
   path: "/",
 }
