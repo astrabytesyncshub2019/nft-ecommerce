04a3e6487c7193962e0201efd6431cc46ae14f33
Mann <randhawamanpreet37@gmail.com>
2025-09-25 00:10:38 +0530

    google auth chal ja yrr

--- server/src/config/cookieOptions.js
+++ server/src/config/cookieOptions.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === "production"
 export const cookieOptionsForAcessToken = {
   httpOnly: true,
   secure: isProduction,               // only require HTTPS in production
-  sameSite:  "none", // "none" allows cross-site in dev
+  sameSite: "none", // "none" allows cross-site in dev
   maxAge: 180 * 60 * 1000,
   path: "/",
 }
@@ -11,7 +11,7 @@ export const cookieOptionsForAcessToken = {
 export const cookieOptionsForRefreshToken = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: isProduction ? "strict" : "none",
+  sameSite:"none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 }
