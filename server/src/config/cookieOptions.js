09ce87301dfb5e3aff3fd78737df86cd88627ec4
Mann <randhawamanpreet37@gmail.com>
2025-09-24 22:56:58 +0530

    changes in cookie options for cross site

--- server/src/config/cookieOptions.js
+++ server/src/config/cookieOptions.js
@@ -1,17 +1,17 @@
-// const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === "production"
 
 export const cookieOptionsForAcessToken = {
   httpOnly: true,
-  secure: true,                    // must be true in prod (HTTPS)
-  sameSite: "strict",                        // "None" required for cross-site
-  maxAge: 180 * 60 * 1000,                 
+  secure: isProduction,               // only require HTTPS in production
+  sameSite: isProduction ? "strict" : "none", // "none" allows cross-site in dev
+  maxAge: 180 * 60 * 1000,
   path: "/",
 }
 
 export const cookieOptionsForRefreshToken = {
   httpOnly: true,
-  secure: true,
-  sameSite: "strict",                        // must be None
-  maxAge: 7 * 24 * 60 * 60 * 1000,         // 7 days
+  secure: isProduction,
+  sameSite: isProduction ? "strict" : "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 }
